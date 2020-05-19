dc3e9f64864b52004b1c6d9aea3b4a6ca2a9b409
CrypticSquirrel <brejae@andrews.edu>
2020-05-18 20:44:00 -0400

    Added authentication middleware
    if a route contains /auth/ it will be caught and handled by this middleware. Errors will be caught by error handers.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,37 @@
+/* ------------------------------------------- Imports ------------------------------------------ */
+
 const express = require('express');
+const auth = require('./auth');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+/* ----------------------------------------- Middleware ----------------------------------------- */
+app.use('/auth', auth);
+
 app.get('/', (req, res) => {
     res.json({
-        message: 'hello!'
+        message: 'Welcome to the Backend!',
     });
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+/* --------------------------------------- Error Handling --------------------------------------- */
+
+app.use(function notFound(req, res, next) {
+    res.status(404);
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    next(error);
+});
+
+app.use(function errorHandler(err, req, res, next) {
+    res.status(res.statusCode || 500);
+    res.json({
+        message: err.message,
+        stack: err.stack,
+    });
+});
+
+/* ---------------------------------------- Code Go Vroom --------------------------------------- */
+
+app.listen(port, () => console.log(`App started at http://localhost:${port}`));
